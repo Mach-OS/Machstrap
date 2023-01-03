9bc49d8243d553f14f5538f237e7edb95d9072be
Christian Chiarulli <chris.machine@pm.me>
2023-01-03 18:19:12 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,133 +1,75 @@
 #!/bin/bash
 
-welcome() {
-  echo "Welcome, to Machstrap, this script will bootstrap Arch linux with programs and development tools to increase your productivity."
-  echo "For this installer to work you will need to have an active internet connection and allow this program to run as root."
+install_base_packages() {
+	cat ./packages/base.list | xargs sudo pacman -S -y
 
-  # TODO solve this
-  # pacman --noconfirm --needed -Sy archlinux-keyring > /dev/null 2>&1 || echo "MUST RUN AS ROOT USER"
+	echo "Are you using a laptop? (y/n)"
+	read -r laptop
+
+	if [[ "$laptop" == "y" ]]; then
+		pacman -S tlp
+		systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+	fi
 }
 
-set_nopasswd_wheel() {
-  sed -i "/#MACHSTRAP/d" /etc/sudoers
-  echo "%wheel ALL=(ALL) NOPASSWD: ALL #MACHSTRAP" >> /etc/sudoers
+install_processor_image() {
+	echo "Do you have an AMD processor? (y/n)"
+	read -r amd
+	if [[ "$amd" == "y" ]]; then
+		pacman -S amd-ucode
+	fi
+
+	echo "Do you have an Intel processor? (y/n)"
+	read -r intel
+	if [[ "$intel" == "y" ]]; then
+		pacman -S intel-ucode
+	fi
 }
 
-add_user() {
-  echo -n "Enter new username: "
-  read username
-  # TODO solve this
-  # if id -u $username >/dev/null 2>$1; then
-  #   echo "User already exists!"
-  # else
-    echo -n "Enter password for new user: "
-    read -s password
-    useradd -m -g wheel "$username"
-    echo "$password" | passwd "$username" --stdin
-  # fi
+install_graphics_drivers() {
+	echo "Do you have an nvidia card? (y/n)"
+	read -r nvidia
+	if [[ "$nvidia" == "y" ]]; then
+		pacman -S nvidia nvidia-utils nvidia-settings
+	fi
+
+	echo "Do you have an amd card? (y/n)"
+	read -r amd
+	if [[ "$amd" == "y" ]]; then
+		pacman -S xf86-video-amdgpu
+	fi
 }
 
-choose_user() {
-  echo "Enter existing username: "
-  read username
+create_user() {
+	echo "Enter a value for the username: "
+	read -r username
+
+	useradd -m $username
+	echo "Enter a value for the user password: "
+	read -r password
+	echo $username:$password | chpasswd
+
+	echo "$username ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers.d/$username
 }
 
-add_or_choose_user() {
-  while true; do
-    echo "Do you need to create a user?"
-    echo "(y) create a user"
-    echo "(n) choose an existing user"
-    echo "(c) cancel"
-    read -p "> " ync
-      case $ync in
-          [Yy]* ) add_user; break;;
-          [Nn]* ) choose_user; break;;
-          [Cc]* ) exit;;
-          * ) echo " ";;
-      esac
-  done
+enable_services() {
+	echo "Do you want to enable bluetooth? (y/n)"
+	read -r bluetooth
+	if [[ "$bluetooth" == "y" ]]; then
+		systemctl enable bluetooth
+	fi
+	echo "Do you want to enable ssh? (y/n)"
+	read -r ssh
+	if [[ "$ssh" == "y" ]]; then
+		systemctl enable sshd
+	fi
+
+	systemctl enable NetworkManager
+	systemctl enable cups
+	systemctl enable avahi-daemon
+	systemctl enable fstrim.timer
+	systemctl enable firewalld
+	systemctl enable acpid
 }
 
-
-install_basics() {
-  pacman --noconfirm --needed -Sy $(< ./packages/base.list)
-}
-
-install_aur_helper() {
-  cd /tmp
-  sudo -u "$username" git clone https://aur.archlinux.org/yay-bin.git
-  cd yay-bin
-  sudo -u "$username" makepkg --noconfirm -si 
-  cd -
-  rm -rf /tmp/yay-bin
-}
-
-install_fonts() {
-  # do you want all the nerdfonts? This is about 2GB of data
-  yay --noconfirm --needed -Sy nerd-fonts-jetbrains-mono
-}
-
-start_network_service() {
-  systemctl start NetworkManager
-  systemctl enable NetworkManager
-}
-
-start_display_manager_service() {
-  systemctl enable lightdm
-}
-
-# reuse this function
-install_extras() {
-  while true; do
-    echo "Do you want to install extras?"
-    cat ./packages/extra.list
-    echo "(y) install extras"
-    echo "(n) don't install extras"
-    read -p "> " ync
-      case $ync in
-          [Yy]* ) sudo -u "$username" yay --noconfirm --needed -Sy $(< $HOME/Machstrap/packages/extra.list); break;;
-          [Nn]* ) echo "Not installing extras"; break;;
-          * ) echo " ";;
-      esac
-  done
-}
-
-install_aur_packages() {
-  sudo -u "$username" yay --noconfirm --needed -Sy $(< $HOME/Machstrap/packages/aur.list)
-}
-
-# install_data_science_package() {
-#   # ask first
-#   sudo pacman -S $(< ./packages/data_science.list)
-# }
-
-# install_web_dev_package() {
-#   # ask first
-#   sudo pacman -S $(< ./packages/web_dev.list)
-# }
-
-# install_normie_package() {
-#   # ask first
-#   sudo pacman -S $(< ./packages/normie.list)
-# }
-
-# install_nvcode() {
-
-# }
-
-welcome
-set_nopasswd_wheel
-add_or_choose_user
-install_basics
-start_network_service
-install_aur_helper
-install_aur_packages
-install_extras
-start_display_manager_service
-make_home_directories_for_user
-# polybar i3 fontconfig alacritty
-
-# wallpapers
-# xprofile
-# zsh
-# zprofile
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
